ba458d58ac9b7d4e272eec069dd1196d96fcf890
serg <shkaev.ss@mail.ru>
2025-04-30 13:19:27 +0300

    create PostRepository

---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package faang.school.postservice.repository;
+
+import faang.school.postservice.model.Post;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+    List<Post> findByAuthorId(long authorId);
+}
