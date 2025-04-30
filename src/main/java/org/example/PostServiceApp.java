05df7c2d0204e1f1d187198fbeb73351a5610423
serg <shkaev.ss@mail.ru>
2025-04-30 13:22:11 +0300

    create PostServiceApp

---
+++ src/main/java/org/example/PostServiceApp.java
@@ -0,0 +1,17 @@
+package faang.school.postservice;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class PostServiceApp {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(PostServiceApp.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+}
