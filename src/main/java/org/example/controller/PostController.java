ae04318101ff27000f8d56326078b3f4d6432afb
serg <shkaev.ss@mail.ru>
2025-05-01 13:18:41 +0300

    feat(PostController) create()

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -1,7 +1,11 @@
 package org.example.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.dto.PostDto;
 import org.example.service.PostService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PostController {
   private final PostService postService;
+
+  @PostMapping()
+  public PostDto create(@Valid @RequestBody PostDto postDto) {
+    return postService.create(postDto);
+  }
 }
