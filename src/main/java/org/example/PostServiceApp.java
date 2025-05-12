1db928689cb01ab61e748bc9d1ff9e1f47fa2d2f
serg <shkaev.ss@mail.ru>
2025-05-12 23:06:10 +0300

    update

--- src/main/java/org/example/PostServiceApp.java
+++ src/main/java/org/example/PostServiceApp.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Map.Entry.comparingByValue;
+
 @SpringBootApplication
 @EnableFeignClients
 public class PostServiceApp {
@@ -14,4 +21,5 @@ public class PostServiceApp {
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
     }
+
 }
