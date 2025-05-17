4dc658baf5913e29efd6b2b8dffbf7465f238b9d
serg <shkaev.ss@mail.ru>
2025-05-17 13:46:11 +0300

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
     java
-//    id("org.springframework.boot") version "3.1.2"
-//    id("io.spring.dependency-management") version "1.1.2"
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "org.example"
-version = "1.0"
-java.sourceCompatibility = JavaVersion.VERSION_17
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
